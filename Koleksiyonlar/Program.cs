3cd9fef8f25936e21fb42f8b789e91c3e5d02193
elif4423 <15290063@firat.edu.tr>
2022-11-20 20:43:39 +0300

    Değişiklik

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace Koleksiyonlar
 {
-    internal class Program
+    public class Program
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] isimler = new string[] {"Elif","Ezgi","Berfin","Zülal" };
+
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            
+            isimler[4] = "Hatice";
+            Console.WriteLine(isimler[4]);
         }
     }
 }
