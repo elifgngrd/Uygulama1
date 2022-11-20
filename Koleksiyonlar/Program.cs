0e59ed409f56fe9e2dffe6d9e319894488f2a455
Elif Güngördü <gungorduelif34@gmail.com>
2022-11-20 23:14:56 +0300

    123

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -10,6 +10,7 @@
             Console.WriteLine(isimler[1]);
             Console.WriteLine(isimler[2]);
             Console.WriteLine(isimler[3]);
+            //deneme
            
         }
     }
